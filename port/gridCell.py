a65a810cdcd3998cb907abf9824a3d4e03ae36bb
Steven Lee-Kramer <sleekramer@gmail.com>
2016-09-25 19:28:09 -0400

    overhaul

--- port/gridCell.py
+++ port/gridCell.py
@@ -15,123 +15,123 @@ import random
 import time
 
 def round_down(num):
-	return num - (num%10)
+    return num - (num%10)
 
 def dump(obj):
-	for attr in dir(obj):
-		print "obj.%s = %s" % (attr, getattr(obj, attr))
-		pass
+    for attr in dir(obj):
+        print "obj.%s = %s" % (attr, getattr(obj, attr))
+        pass
 
 class board(Widget):
-	# sound = SoundLoader.load('img/emotion.wav')
-	# if sound:
-	# 	sound.loop = True
-	# 	sound.play()
+    # sound = SoundLoader.load('img/emotion.wav')
+    # if sound:
+    #     sound.loop = True
+    #     sound.play()
 
-	cells = {}
-	count = 0
-	# gets the overall window.size of the screen
-	screen = Window.size
-	w, h = round_down(screen[0]),int(round_down(screen[1] * .8))
-	# portion of screen dedicated to the widgets
-	btns = int(round_down(screen[1] * .2))
+    cells = {}
+    count = 0
+    # gets the overall window.size of the screen
+    screen = Window.size
+    w, h = round_down(screen[0]),int(round_down(screen[1] * .8))
+    # portion of screen dedicated to the widgets
+    btns = int(round_down(screen[1] * .2))
 
 
 
-	def drawGrid(self):
-		with self.canvas:
-			for xx in range(0,self.w-10,10):
-				for yy in range(0,self.h,10):
-					self.cells.update({(xx,yy):0})
-					
-					stat = random.randint(0,1)
-					if stat == 1:
-						self.color = Color(1,1,1)
-					else:
-						self.color = Color(0,0,0)
-					self.rect = Rectangle(pos=(5+xx,int(self.btns/2.)+yy), size=(9,9))
-					self.cells.update({(xx,yy):[self.rect,self.color,stat]})
-		return self.cells
+    def drawGrid(self):
+        with self.canvas:
+            for xx in range(0,self.w-10,10):
+                for yy in range(0,self.h,10):
+                    self.cells.update({(xx,yy):0})
+                    
+                    stat = random.randint(0,1)
+                    if stat == 1:
+                        self.color = Color(1,1,1)
+                    else:
+                        self.color = Color(0,0,0)
+                    self.rect = Rectangle(pos=(5+xx,int(self.btns/2.)+yy), size=(9,9))
+                    self.cells.update({(xx,yy):[self.rect,self.color,stat]})
+        return self.cells
 
-	def update_cell(self, *args):
-		print "Passed"
-		for cell in self.cells:
-			self.cells[cell][2] == random.randint(0,1)
-			if self.cells[cell][2] == 1:
-				self.cells[cell][1].rgb = [1,1,1]
-			else:
-				self.cells[cell][1].rgb = [0,0,0]
+    def update_cell(self, *args):
+        print "Passed"
+        for cell in self.cells:
+            self.cells[cell][2] == random.randint(0,1)
+            if self.cells[cell][2] == 1:
+                self.cells[cell][1].rgb = [1,1,1]
+            else:
+                self.cells[cell][1].rgb = [0,0,0]
 
 class boardWidget(GridLayout):
-	def __init__(self, **kwargs):
-		super(boardWidget, self).__init__(**kwargs)
-		self.screen = Window.size
-		self.w, self.h = self.screen[0],self.screen[1]
-		self.cols = 20
-		self.rows = 10
-		self.padding = 5
-		self.pos = (0,int(self.h*.1))
-		
-		for i in range(0,200):
-			btn = Button(background_color=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
-			self.add_widget(btn)
+    def __init__(self, **kwargs):
+        super(boardWidget, self).__init__(**kwargs)
+        self.screen = Window.size
+        self.w, self.h = self.screen[0],self.screen[1]
+        self.cols = 20
+        self.rows = 10
+        self.padding = 5
+        self.pos = (0,int(self.h*.1))
+        
+        for i in range(0,200):
+            btn = Button(background_color=(random.randint(0,255),random.randint(0,255),random.randint(0,255)))
+            self.add_widget(btn)
 
 
 
 class RootWidget(FloatLayout):
 
-	def __init__(self, **kwargs):
-		super(RootWidget, self).__init__(**kwargs)
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
 
-		self.btns = [controls.sett, controls.info, controls.reset, controls.start, controls.stop, controls.prsts]
-		for item in self.btns:
-			self.add_widget(item)
+        self.btns = [controls.sett, controls.info, controls.reset, controls.start, controls.stop, controls.prsts]
+        for item in self.btns:
+            self.add_widget(item)
 
-		w = boardWidget()
-		dump(w)
-		self.add_widget(w)
+        w = boardWidget()
+        dump(w)
+        self.add_widget(w)
 
-		# grid = boardWidget()
-		# self.add_widget(grid)
-		# Adds the area for the cells
-		# theboard = board()
-		# self.add_widget(theboard)
+        # grid = boardWidget()
+        # self.add_widget(grid)
+        # Adds the area for the cells
+        # theboard = board()
+        # self.add_widget(theboard)
 
 
-		# cells = theboard.drawGrid()
-		# print cells
+        # cells = theboard.drawGrid()
+        # print cells
 
 
 
 
 class MyApp(App):
 
-	def build(self):
-		'''Older version'''
-		self.root = root = RootWidget()
-		
-		# test = RootWidget()
+    def build(self):
+        '''Older version'''
+        self.root = root = RootWidget()
+        
+        # test = RootWidget()
 
-		# test.update_cell()
+        # test.update_cell()
 
-		return self.root
+        return self.root
 
-		'''New version?'''
-		# self.layout = RootWidget()
+        '''New version?'''
+        # self.layout = RootWidget()
 
-		# theboard = board()
+        # theboard = board()
 
-		# self.layout.add_widget(theboard)
+        # self.layout.add_widget(theboard)
 
-		# cells = theboard.drawGrid()
-		# theboard.update_cell()
+        # cells = theboard.drawGrid()
+        # theboard.update_cell()
 
-		# Clock.schedule_interval(theboard.update_cell, 0.5)
+        # Clock.schedule_interval(theboard.update_cell, 0.5)
 
-		# return self.layout
+        # return self.layout
 
 
 
 
 if __name__=="__main__":
-	MyApp().run()
+    MyApp().run()
--- port/main_for_dev.py
+++ port/main_for_dev.py
@@ -14,106 +14,106 @@ from functionality import controls
 import random
 
 # Considerations for user end:
-	# Allow user to toggle between the shapes being 'Ellipse' or 'Rectangle' or ??? (Kivy terminology)
-	# Allow user to adjust the rules for cells to die, live, or become alive
+    # Allow user to toggle between the shapes being 'Ellipse' or 'Rectangle' or ??? (Kivy terminology)
+    # Allow user to adjust the rules for cells to die, live, or become alive
 
 def round_down(num):
-	return num - (num%10)
+    return num - (num%10)
 
 def dump(obj):
-	for attr in dir(obj):
-		print "obj.%s = %s" % (attr, getattr(obj, attr))
-		pass
+    for attr in dir(obj):
+        print "obj.%s = %s" % (attr, getattr(obj, attr))
+        pass
 
 class board(Widget):
-	sound = SoundLoader.load('img/emotion.wav')
-	if sound:
-		sound.loop = True
-		sound.play()
+    sound = SoundLoader.load('img/emotion.wav')
+    if sound:
+        sound.loop = True
+        sound.play()
 
-	cells = {}
-	count = 0
-	screen = Window.size
-	w, h = round_down(screen[0]),round_down(screen[1])
+    cells = {}
+    count = 0
+    screen = Window.size
+    w, h = round_down(screen[0]),round_down(screen[1])
 
-	def drawGrid(self):
-		# screen = Window.size
-		# w, h = round_down(screen[0]),round_down(screen[1])
+    def drawGrid(self):
+        # screen = Window.size
+        # w, h = round_down(screen[0]),round_down(screen[1])
 
-		with self.canvas:
-			for xx in range(0,self.w-10,10):
-				for yy in range(0,self.h-100,10):
-					self.cells.update({(xx,yy):0})
-					
-					stat = random.randint(0,1)
-					self.color = Color(1,1,1)
-					self.rect = Rectangle(pos=(5+xx,50+yy), size=(9,9))
-					self.cells.update({(xx,yy):[self.rect,self.color,stat]})
-		return self.cells
+        with self.canvas:
+            for xx in range(0,self.w-10,10):
+                for yy in range(0,self.h-100,10):
+                    self.cells.update({(xx,yy):0})
+                    
+                    stat = random.randint(0,1)
+                    self.color = Color(1,1,1)
+                    self.rect = Rectangle(pos=(5+xx,50+yy), size=(9,9))
+                    self.cells.update({(xx,yy):[self.rect,self.color,stat]})
+        return self.cells
 
-	def update_cell(self, *args):
-		for cell in self.cells:
-			self.cells[cell][2]=random.randint(0,1)
-			if self.cells[cell][2] == 1:
-				self.cells[cell][1].rgb = [1,1,1]
-			else:
-				self.cells[cell][1].rgb = [0,0,0]
+    def update_cell(self, *args):
+        for cell in self.cells:
+            self.cells[cell][2]=random.randint(0,1)
+            if self.cells[cell][2] == 1:
+                self.cells[cell][1].rgb = [1,1,1]
+            else:
+                self.cells[cell][1].rgb = [0,0,0]
 
-	def on_touch_down(self, touch):
-		xtouch = int(round_down(touch.x)-10)
-		ytouch = int(round_down(touch.y)-50)
-		sound = SoundLoader.load('img/button2.wav')
-		if sound:
-			sound.volume = 0.6
-			sound.play()
+    def on_touch_down(self, touch):
+        xtouch = int(round_down(touch.x)-10)
+        ytouch = int(round_down(touch.y)-50)
+        sound = SoundLoader.load('img/button2.wav')
+        if sound:
+            sound.volume = 0.6
+            sound.play()
 
-		try: 
-			self.cells[(xtouch,ytouch)][1].rgb = [1,0,0]
-		except:
-			pass
+        try: 
+            self.cells[(xtouch,ytouch)][1].rgb = [1,0,0]
+        except:
+            pass
 
-	def logic(self, *args):
-		for cell in self.cells:
-			alive = 0
-			for xx in [-10,10,0]:
-				for yy in [-10,10,0]:
-					if (xx,yy) != (0,0):
-						try:
-							alive += self.cells[cell][2]
-						except KeyError:
-							pass
-					else:
-						pass
+    def logic(self, *args):
+        for cell in self.cells:
+            alive = 0
+            for xx in [-10,10,0]:
+                for yy in [-10,10,0]:
+                    if (xx,yy) != (0,0):
+                        try:
+                            alive += self.cells[cell][2]
+                        except KeyError:
+                            pass
+                    else:
+                        pass
 
-			if self.cells[cell][2] == 1:
-				if alive < 2 or alive > 3:
-					self.cells[cell][2] = 0
-				else:
-					self.cells[cell][2] = 1
-			else:
-				if alive == 3:
-					self.cells[cell][2] = 1
-					print self.cells[cell][2]
+            if self.cells[cell][2] == 1:
+                if alive < 2 or alive > 3:
+                    self.cells[cell][2] = 0
+                else:
+                    self.cells[cell][2] = 1
+            else:
+                if alive == 3:
+                    self.cells[cell][2] = 1
+                    print self.cells[cell][2]
 
 
 
 class mainGameApp(App):
-	# Use this if you want to test different sizes of the window...
-	# Right now it changes dynamically depending on the system.
-	# Window.size = (1200,1200)
-	
-	def build(self):
-		# self.layout = FloatLayout()
-		# self.layout.add_widget(controls.start)
-		cells = board()
-		allcells = cells.drawGrid()
-		
-		#Figure out a way to allow the user to select the time frame (0->1)
-		
-		Clock.schedule_interval(cells.update_cell, 0.5)
-		return cells
+    # Use this if you want to test different sizes of the window...
+    # Right now it changes dynamically depending on the system.
+    # Window.size = (1200,1200)
+    
+    def build(self):
+        # self.layout = FloatLayout()
+        # self.layout.add_widget(controls.start)
+        cells = board()
+        allcells = cells.drawGrid()
+        
+        #Figure out a way to allow the user to select the time frame (0->1)
+        
+        Clock.schedule_interval(cells.update_cell, 0.5)
+        return cells
 
 if __name__ == '__main__':
-	mainGameApp().run()
+    mainGameApp().run()
 
 
